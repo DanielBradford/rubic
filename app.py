09bae3034a223a3f1eeb945e3890d57b279733ae
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-09-02 15:24:15 +0000

    Login funcitonality and profile routing added

--- app.py
+++ app.py
@@ -24,14 +24,40 @@ def home():
     return render_template("landing.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"user_name": request.form.get("user_name").lower()
+             })
+
+        if existing_user:
+            if check_password_hash(existing_user["password"],
+                                   request.form.get("password")):
+                session["user"] = request.form.get("user_name").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("user_name")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+
+                return render_template("profile.html")
+
+            else:
+                flash("Incorrect username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
+
 
 
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/recipes")
 def recipes():
@@ -47,13 +73,14 @@ def add_recipe():
     recipes = list(mongo.db.type.find().sort("type", 1))
     return render_template("add_recipe.html", recipes=recipes)
 
+
 @app.route("/add_user", methods=["GET", "POST"])
 def add_user():
     if request.method == "POST":
         password = request.form.get("password")
         confirm = request.form.get("confirm")
         existing_user = mongo.db.users.find_one(
-                        {"user_name": request.form.get("user_name").lower()})
+            {"user_name": request.form.get("user_name").lower()})
 
         if existing_user:
             flash("Username already exists")
@@ -78,23 +105,20 @@ def add_user():
         session["user"] = request.form.get("user_name")
         flash("Registration Successful!")
         return redirect(url_for(
-        "profile", username=session["user"]))
+            "profile", username=session["user"]))
     return render_template("landing.html")
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab session users username from database
+    username = mongo.db.users.find_one(
+        {"user_name": session["user"]})['username']
 
+    if session["user"]:
+        return render_template("profile.html", username=username)
 
-# @app.route("/categories")
-# def categories():
-
-#     types = list(mongo.db.type.find())
-
-#     return render_template("categories.html", types=types)
-
-
+    return redirect(url_for('login'))
 
 
 @app.route("/add_new_recipe", methods=["GET", "POST"])
