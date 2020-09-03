ceaba010a5d70e7390568f1a42ce9d33fb1c2337
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-09-03 11:13:43 +0000

    Logout confirmation check introduced to js

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,19 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $("select").formSelect();
+  $(".chips").chips();
+});
 
- $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-     $('.chips').chips();
-  });
+// $(".card").click(function () {
+//   $(".card").hide();
+//   $(this).show();
+//   $(this).addClass("categoryTitle");
+// });
+
+//   $("#logout").click(function () {
+//     confirm("Are you sure you want to logout?");
+//     if (a == true) {
+//     } else {
+//       return false
+//     }
+//   });
