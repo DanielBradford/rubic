583d4bf05b9d3ec236d75e731483ccd7acd83e27
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-09-04 15:48:31 +0000

    Delete confirmation and logout confirmation added for double verification in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
+  $("select").formSelect();
   $(".collapsible").collapsible();
-//   $(".sidenav").sidenav();
-//   $("select").formSelect();
+  $(".sidenav").sidenav();
+  $("#logout").click(function () {
+    if (confirm("Are you sure you want to logout?")) {
+    } else {
+      return false; //----On cancel user returns to current screen------//
+    }
+  });
+  /*------Delete Button Confirmation-----*/
+  $('.delete-btn').click(function () {
+    if (confirm("Are you sure you want to DELETE this recipe? This action cannot be undone!")) {
+    } else {
+      return false; //----On cancel user returns to current screen------//
+    }
+  });
 });
-
