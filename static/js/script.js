aeff64e8cc22b490d9dae9dfad628a4753dac13a
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-09-05 19:03:10 +0000

    Delete btn, back btn and remove btn functionality added with user confirm box when needed

--- static/js/script.js
+++ static/js/script.js
@@ -4,13 +4,36 @@ $(document).ready(function () {
   $(".sidenav").sidenav();
   $("#logout").click(function () {
     if (confirm("Are you sure you want to logout?")) {
+      alert("YOU HAVE BEEN SUCCESSFULLY LOGGED OUT");
     } else {
       return false; //----On cancel user returns to current screen------//
     }
   });
   /*------Delete Button Confirmation-----*/
-  $('.delete-btn').click(function () {
-    if (confirm("Are you sure you want to DELETE this recipe? This action cannot be undone!")) {
+  $(".delete-btn").click(function () {
+    if (
+      confirm(
+        "Are you sure you want to DELETE this recipe? This action cannot be undone!"
+      )
+    ) {
+      alert("RECIPE HAS BEEN SUCCESSFULLY DELETED");
+    } else {
+      return false; //----On cancel user returns to current screen------//
+    }
+  });
+
+  //   back button for breadcrumb nav
+
+  $("#back").click(function () {
+    window.history.back();
+  });
+//   remove button for saved recipes
+  $(".remove-btn").click(function () {
+    if (
+      confirm(
+        "Are you sure you want to REMOVE this recipe from the SAVED RECIPES? This action cannot be undone!"
+      )
+    ) {alert("RECIPE HAS BEEN SUCCESSFULLY REMOVED")
     } else {
       return false; //----On cancel user returns to current screen------//
     }
