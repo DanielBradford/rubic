74df926006ccfed1cfdbb2cc9647a4c7427e0945
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-09-13 12:30:31 +0000

    Notes for functions added and minor icon change functions added

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
+  // materialise js
   $("select").formSelect();
   $(".collapsible").collapsible();
   $(".dropdown-trigger").dropdown();
   $(".sidenav").sidenav();
+
+  //   logout confirm function
   $("#logout").click(function () {
     if (confirm("Are you sure you want to logout?")) {
     } else {
       return false; //----On cancel user returns to current screen------//
     }
   });
-  /*------Delete Button Confirmation-----*/
+  //   /*------Delete Button Confirmation-----*/
+  //   confirms the user wants to delete the recipe
   $(".delete-btn").click(function () {
     if (
       confirm(
@@ -24,14 +28,8 @@ $(document).ready(function () {
 
   //   back button for breadcrumb nav
 
-  $("#back").click(function () {
+  $(".back").click(function () {
     window.history.back();
-  });
-
-  $("#save-btn").click(function () {
-    alert("This recipe has been SAVED successfully!");
-    $(".lever").addAttribute("selected");
-    $("#save-text").text("SAVED");
   });
 
   //   remove button for saved recipes
@@ -39,6 +37,7 @@ $(document).ready(function () {
     $(".remove").toggle("left");
   });
 
+  //   the above presents the following to the user
   $(".remove").click(function () {
     if (
       confirm(
@@ -59,6 +58,12 @@ $(document).ready(function () {
     $(this).removeClass("pulse");
   });
 
+//   random button border add
+$("#random-btn").click(function(){
+    $(".collection-item").addClass("random-border");
+
+});
+
   // Shows all recipes
   $("#allRecipes").click(function () {
     $(".recipe-cards").attr("hidden", false);
@@ -75,30 +80,36 @@ $(document).ready(function () {
   });
 
   //   management template
+  //   all user button
   $("#all_users").click(function () {
     $("#user-table").toggle("slow");
     $("#types-table").hide("slow");
   });
 
+  // recipes button
   $("#recipes-btn").click(function () {
     $("#recipe-table").toggle("slow");
     $("#user-table").hide("slow");
     $("#types-table").hide("slow");
   });
 
+  // recipe types button
   $("#types-btn").click(function () {
     $("#types-table").show("slow");
     $("#recipe-table").hide("slow");
     $("#user-table").hide("slow");
   });
 
+  // email confirmation
   $("#email_link").click(function () {
     if (confirm("Would you like to Email this User?")) {
     } else {
       return false; //----On cancel user returns to current screen------//
     }
   });
-  $("#delete-user-btn").click(function () {
+
+  // delete user button
+  $(".delete-user-btn").click(function () {
     if (
       confirm(
         "Are you sure you want to delete this user? This action cannot be undone!"
@@ -108,6 +119,8 @@ $(document).ready(function () {
       return false; //----On cancel user returns to current screen------//
     }
   });
+
+  // delete type button
   $("#delete-type-btn").click(function () {
     if (
       confirm(
@@ -118,13 +131,19 @@ $(document).ready(function () {
       return false; //----On cancel user returns to current screen------//
     }
   });
+
+  //   add type button
   $("#add_type_show").click(function () {
+    //   changes drop down icon to exit icon
+    $(".fa-chevron-down").toggle();
+    $(".fa-times").toggle();
     $("#add_type").slideToggle("slow");
   });
 
   //   searchbars
   $(".search").click(function () {
-    $(".inner-search").slideToggle("slow");
+    $("#search-glass").toggle("medium");
+    $(".fa-times").toggle("medium");
+    $(".inner-search").fadeToggle("medium");
   });
-
 });
